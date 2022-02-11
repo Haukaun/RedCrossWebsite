74a82c00b81825ee4fb22186d6c796cde82e0b29
Haukaun <haakonfs@hotmail.com>
2022-02-11 16:06:18 +0100

    Added my project

---
+++ script.js
@@ -0,0 +1,16 @@
+const togglebutton = document.getElementsByClassName('toggle-button')[0]
+
+const menubar = document.getElementsByClassName('menubar-links')[0]
+
+/* const body = document.getElementsByTagName('body')[0] */
+
+
+togglebutton.addEventListener('click',() => {
+    menubar.classList.toggle('active')
+})
+
+/* body.addEventListener('click', () => {
+menubar.classList.toggle('close')
+}) */
+
+
